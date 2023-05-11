6ef89ba8ddaf25fac6524d102487a8da64f009fa
renata-aline <renataaline@gmail.com>
2023-05-11 17:36:29 -0300

    operador lógico

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
-
-function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+function tocaSom(idElementoAudio) {
+  document.querySelector(idElementoAudio).play();
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll(".tecla");
 
-for (let contador = 0; contador < listaDeTeclas.length; contador++){
-   
-    const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`;
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`;
 
-    tecla.onclick = function (){
-        tocaSom(idAudio);
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  };
+  tecla.onkeydown = function (evento) {
+    if (evento.code === "Space" || evento.code === "Enter") {
+      tecla.classList.add("ativa");
     }
-    tecla.onkeydown = function (evento) {
-        if (evento.code === 'space'){
-       tecla.classList.add('ativa');
-        } 
-    }
-    tecla.onkeyup = function () {
-        tecla.classList.remove('ativa');
-    }
+  };
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
+  };
 }
